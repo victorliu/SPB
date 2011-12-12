44f98de7c962e20a6206915dde58a1072527b961
Victor Liu <victor.liu@gmail.com>
2011-12-11 23:39:43 -0800

    Adding an example input file. This is just a design goal.

---
+++ input.lua
@@ -0,0 +1,43 @@
+S = SPB.NewSimulation{
+	Lattice = {
+		{1,0},
+		{0,1}
+	},
+	Polarization = 'E',
+	NumBands = 10,
+	Resolution = 20,
+	TargetFrequency = 0.1,
+	Tolerance = 1e-7;
+}
+
+S:AddMaterial{
+	Name = 'Metal',
+	EpsilonInf = {1,0},
+	LorentzPoles = {
+		{
+			Omega0 = 1,
+			Gamma = 0,
+			OmegaP = 0
+		}
+	}
+}
+
+S:AddRectangle{
+	Material = 'Metal',
+	Center = {0,0},
+	Halfwidths = {0.25,0.25},
+	Angle = 0
+}
+
+S:SolveK{0,0}
+
+print(unpack(S:GetFrequencies()));
+
+--[[
+band1 = S:GetBand(1);
+band2 = S:GetBand(2);
+dot = S:InnerProduct(band1,band2)
+band1:OutputHDF{
+	BaseName = 'field_'
+}
+]]
