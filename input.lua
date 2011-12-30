26bbe3aaaf8bc4df1b5a1f34ab4ec990ac824e80
Victor Liu <victor.liu@gmail.com>
2011-12-30 05:29:30 -0800

    Updated input file format. More explicit, and only allows frequency range solves.

--- input.lua
+++ input.lua
@@ -1,3 +1,4 @@
+
 S = SPB.NewBandSolver{
 	Lattice = {
 		{1,0},
@@ -5,49 +6,39 @@ S = SPB.NewBandSolver{
 	},
 	Polarization = 'E'
 }
-scale = 2*math.pi
-
 
 S:SetOptions{
 	NumBands = 30,
 	Resolution = {10, 10},
-	TargetFrequency = 0. * scale,
+	TargetFrequencyRange = {0.2, 0.6},
+	ApproximationTolerance = 1e-4,
 	Tolerance = 1e-7,
 	Verbosity = 0
 }
 
 S:AddMaterial{
 	Name = 'Si',
-	EpsilonInf = {12,0}
+	EpsilonInfinity = {12,0}
 }
 
-S:AddMaterial{
-	Name = 'Diel',
-	EpsilonInf = {2.3646,0}
-}
 S:AddMaterial{
 	Name = 'Silver',
-	EpsilonInf = {2.3646,0}
+	EpsilonInfinity = 2.3646,
+	LorentzPoles = {
+		{
+			ResonanceFrequency = {0.4593, 0.0587/2},
+			PlasmaFrequency = 0.1676
+		},
+		{
+			ResonanceFrequency = {0.5434, 0.115/2},
+			PlasmaFrequency = 0.3293
+		},
+		{
+			ResonanceFrequency = {0, 0.0079/2},
+			PlasmaFrequency = 0.6253
+		}
+	}
 }
-S:AddMaterialLorentzPole{
-	Material = 'Silver',
-	Omega0 = 0.4593 * scale,
-	Gamma = 0,
-	OmegaP = 0.1676 * scale
-}
-S:AddMaterialLorentzPole{
-	Material = 'Silver',
-	Omega0 = 0.5434 * scale,
-	Gamma = 0,
-	OmegaP = 0.3293 * scale
-}
-S:AddMaterialLorentzPole{
-	Material = 'Silver',
-	Omega0 = 0,
-	Gamma = 0,
-	OmegaP = 0.6253 * scale
-}
-
 
 S:SetRectangle{
 	Material = 'Silver',
@@ -63,13 +54,13 @@ S:OutputEpsilon{
 }
 os.exit(0)
 ]]
-for kx = 0.01,0.5,0.01 do
+for kx = 0,0.5,0.01 do
 	S:SolveK{kx,0}
 
 	io.stdout:write(kx)
 	for i,freq in ipairs(S:GetFrequencies()) do
 		if freq[1] >= 0 then
-			io.stdout:write('\t' .. freq[1]/scale)
+			io.stdout:write('\t' .. freq[1])
 		end
 	end
 	io.stdout:write('\n')
